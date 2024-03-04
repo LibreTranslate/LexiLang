86d1c6976bf08608f110e2e5695977a67396c47d
terrtia <or1994@hotmail.fr>
2024-03-04 11:24:28 +0100

    chg [detector]: replace punctuations with whitespace

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from lexilang.detector import detect
 
-print(detect("bonjour")) # ('fr', 0.45)
+print(detect("bonjour"))   # ('fr', 0.45)
+print(detect("bonjour!"))  # ('fr', 0.45)
 print(detect("学中文")) # ('zh', 0.45)
 print(detect("ciao mondo")) # ('it', 0.9)
 print(detect("El gato doméstico")) # ('es', 0.45)
+print(detect("El\"gato\",doméstico")) # ('es', 0.45)
 print(detect("ciao", languages=["de", "ro"])) # ('de', 0.45)
